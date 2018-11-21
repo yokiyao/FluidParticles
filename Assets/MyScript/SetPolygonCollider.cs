ee87973418bbafa5d1fc6e755a244346afadb940
yoki.yao <yoki.yao@yucolab.com>
2018-11-21 18:51:57 +0800

    depth data controls sphere 2d colliders. ps with trails (satisfied)

---
+++ Assets/MyScript/SetPolygonCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPolygonCollider : MonoBehaviour {
+
+    PolygonCollider2D pc;
+	// Use this for initialization
+	void Awake () {
+        pc = GetComponent<PolygonCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(pc.points[0]);
+    }
+
+
+    public void CreatePolygonShape(List<Vector3> vpos)
+    {
+        if (vpos.Count != 0)
+        {
+            for (int i = 0; i < vpos.Count; i++)
+            {
+                pc.points[i].x = vpos[i].x;
+                pc.points[i].y = vpos[i].y;
+            }
+        }
+        
+    }
+}
